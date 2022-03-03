21e6e033dece2cb72636931dcb976a346b8f54cd
MaHDiaLaGaB <elmahdi.alagab@gmail.com>
2022-03-03 17:07:11 +0300

    implement scrap_image() and scrap_video()

--- scrap.py
+++ scrap.py
@@ -10,17 +10,35 @@ from facebook_scraper import get_posts
 
 class Post(object):
 
-    def __init__(self, page_name):
+    def __init__(self, page_name, num_pages):
         self.page_name = page_name
+        self.num_pages = num_pages
 
-    def scrap(self):
-        for post in get_posts(self.page_name, pages=1):
-            print(post['text'][:50])
+    def scrap_post_text(self):
+        for post in get_posts(self.page_name, pages=self.num_pages):
+            post_text = post['post_text']
+            if post_text:
+                return post_text
+            else:
+                return 'No text in this post'
 
-    def scrap_photo(self):
-        pass
+    def scrap_image(self):
+        for post in get_posts(self.page_name, pages=self.num_pages):
+            post_image = post['image']
+            if post_image:
+                return post_image
+            else:
+                return 'The post without image'
+
+    def scrap_video(self):
+        for post in get_posts(self.page_name, pages=self.num_pages, youtube_dl=False):
+            post_video = post['video']
+            if post_video:
+                print(post_video)
 
 
 # testing
-face = Post('ign')
-face.scrap()
+face = Post('ign', num_pages=3)
+#face.scrap_post_text()
+face.scrap_image()
+# face.scrap_video()
