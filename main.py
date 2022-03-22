49e6ae5d717d52a812e7890b011c1c734d8a4f62
MaHDiaLaGaB <elmahdi.alagab@gmail.com>
2022-03-22 17:55:46 +0300

    add check connection, removing redundancy from Post class

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
-from scrap import Post
+from scrap import Post, check_connection
 from facebook_scraper import exceptions
 
-facebook = Post('ign', 10)
+facebook = Post('ign', 2)
+
+
+check_connection('https://www.facebook.com')
 
 
 # I want to learn more about error handling with scrapping
 # and where I should use them here or in scrap.py
 def grab_facebook():
     try:
-        facebook.scrap_full_post()
+        for i, j in enumerate(facebook.scrape_full_post()):
+            print(f"{i} - {j}")
     except exceptions.NotFound:
         print("Post, page or profile not found / doesn't exist / deleted")
     finally:
--- scrap.py
+++ scrap.py
@@ -1,12 +1,30 @@
 from facebook_scraper import get_posts
+import requests
 import time
 
 """
-    here will be the main objects and methods to scrap the facebook pages
-    the idea is init all the required info from the user like 
-    (page name, how many post, with comments and reactions or not,
-    with videos and photos or not)
+    here will be the main objects and methods to 
+    scrap the facebook pages the idea is init require 
+    two arguments the page name and how many page you 
+    want to scrape
+    
 """
+
+
+def check_connection(url):
+    try:
+        # Get Url
+        get = requests.get(url)
+        # if the request succeeds
+        if get.status_code == 200:
+            print(f"{url}: is reachable")
+        else:
+            print(f"{url}: is Not reachable, status_code: {get.status_code}")
+
+    # Exception
+    except requests.exceptions.RequestException as e:
+        # print URL with Errs
+        raise SystemExit(f"{url}: is Not reachable \nErr: {e}")
 
 
 class Post(object):
@@ -17,45 +35,15 @@ class Post(object):
         self.num_pages = num_pages
         self.post_info = {}
 
-    # scrap post text
-    def scrap_post_text(self):
-        for post in get_posts(self.page_name, pages=self.num_pages):
-            time.sleep(2)
-            post_text = post['post_text']
-            if post_text:
-                print(post_text)
-            else:
-                return 'No text in this post'
-
-    # scrap post image
-    def scrap_image(self):
-        for post in get_posts(self.page_name, pages=self.num_pages):
-            time.sleep(2)
-            post_image = post['image']
-            if post_image:
-                return post_image
-            else:
-                return 'The post without image'
-
-    # scrap post time
-    def scrap_post_date(self):
-        for post in get_posts(self.page_name, pages=self.num_pages):
-            time.sleep(2)
-            date = post['time']
-            time_stamp = post['timestamp']
-            if date and time_stamp:
-                print(date, time_stamp)
-            else:
-                return None
-
     # will scrap all the text, photo, and time in dict
-    def scrap_full_post(self):
+    def scrape_full_post(self):
+        keys = ['text', 'photo', 'time']
+
         for post in get_posts(self.page_name, pages=self.num_pages):
             time.sleep(2)
-            self.post_info['text'] = post['post_text']
-            self.post_info['photo'] = post['image']
-            self.post_info['time'] = post['time']
+            values = [post['post_text'], post['image'], post['time']]
+            self.post_info = dict(zip(keys, values))
             if self.post_info:
-                print(self.post_info)
+                yield self.post_info
             else:
-                return None
+                raise Exception("Scraping failure, check your internet connection")
