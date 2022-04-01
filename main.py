39048ef5d6ab3d50561a6650f162526dbc72531c
MaHDiaLaGaB <elmahdi.alagab@gmail.com>
2022-04-01 16:03:08 +0300

    fix error

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from scrap import Post
+from conftest import post as db
 
 facebook = Post('ign', 10)
 
 
 def grab_facebook():
     for i, j in enumerate(facebook.scrape_full_post()):
-        if facebook.resume_scarping(j):
+        if facebook.resume_scarping(j, db):
             print(f"{i} - {j}")
 
 
