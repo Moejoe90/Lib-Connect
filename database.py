fd47893d231f20567a2e5cedd2f939b5247f5d15
MaHDiaLaGaB <elmahdi.alagab@gmail.com>
2022-04-17 03:44:29 +0300

    create nodes

--- database.py
+++ database.py
@@ -1,26 +1,47 @@
 from settings import NEO4J_USER, NEO4J_URI, NEO4J_PASSWORD
-from py2neo import Node, Graph, Relationship, Subgraph
+from neo4j import GraphDatabase, exceptions
 
 
 class DataBase:
 
     def __init__(self):
-        self.driver = Graph(NEO4J_URI, auth=(NEO4J_USER, NEO4J_PASSWORD))
+        self.driver = GraphDatabase.driver(uri=NEO4J_URI, auth=(NEO4J_USER, NEO4J_PASSWORD))
 
     def close(self):
-        pass
+        self.driver.close()
 
-    def create_nodes(self, page, post, time):
+    def check_connection(self):
+        self.driver.verify_connectivity()
 
-        properties = {"time": f"{time}"}
-        page_node = Node("Page", name=f"{page}")
-        post_node = Node("Post", text=f"{post}")
-        relationship = Relationship(page_node, "Date", post_node, **properties)
-        nodes = [page_node, post_node]
-        subgraph = Subgraph(nodes, relationship)
-        tx = self.driver.begin()
-        tx.create(subgraph)
-        self.driver.commit(tx)
+    def add_page(self, name):
+        with self.driver.session() as session:
+            try:
+                session.write_transaction(self.create_page_node, name)
+                print(f"the first node named {name} has been created")
+            except Exception(exceptions.SessionExpired):
+                raise
+
+    def add_post(self, post_text):
+        with self.driver.session() as session:
+            try:
+                session.write_transaction(self.create_post_node, post_text)
+                print(f"the post node has been created")
+            except Exception(exceptions.SessionExpired):
+                raise
+
+    @staticmethod
+    def create_page_node(tx, name):
+        try:
+            return tx.run("CREATE (a:Page {name: $name})", name=name)
+        except Exception(exceptions.CypherSyntaxError):
+            raise
+
+    @staticmethod
+    def create_post_node(tx, post_text):
+        try:
+            return tx.run("CREATE (b:Post {Name: 'Post', Text: $text})", text=post_text)
+        except Exception(exceptions.CypherSyntaxError):
+            raise
 
 
 
