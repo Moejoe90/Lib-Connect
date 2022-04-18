2cf43cb2708e18d4d68e3bcae622a7bafa6cf6d8
MaHDiaLaGaB <elmahdi.alagab@gmail.com>
2022-04-18 19:09:09 +0300

    create temp scraped data

--- conftest.py
+++ conftest.py
@@ -1,7 +1,22 @@
 import datetime
 
-post = {
+face = [{
     'text': 'Everything you need to know to preorder Hogwarts Legacy, the upcoming open-world RPG set in '
             'the wizarding world of Harry Potter, before the hero arrived.',
     'photo': 'https://assets-prd.ignimgs.com/2022/03/18/hogwarts-legacy-4-1647622573681.jpeg?width=1280',
-    'time': datetime.datetime(2022, 3, 24, 16, 31, 9)}
+    'time': datetime.datetime(2022, 3, 24, 16, 31, 9)}, {
+    'text': ' the upcoming open-world RPG set in '
+            'the wizarding world of Harry Potter, before the hero arrived.',
+    'photo': 'https://assets-prd.ignimgs.com/2022/03/18/hogwarts-legacy-4-1647622573681.jpeg?width=1280',
+    'time': datetime.datetime(2022, 2, 24, 20, 31, 9)},
+    {
+        'text': 'Everything you need to know to preorder Hogwarts Legacy, the upcoming open-world RPG set in '
+                'the wizarding',
+        'photo': 'https://assets-prd.ignimgs.com/2022/03/18/hogwarts-legacy-4-1647622573681.jpeg?width=1280',
+        'time': datetime.datetime(2022, 3, 4, 12, 31, 9)}
+]
+
+for post in face:
+    pass
+
+
