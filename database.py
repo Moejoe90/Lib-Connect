65f0d9865fd67a44549830ab37f1fe60830a684f
MaHDiaLaGaB <elmahdi.alagab@gmail.com>
2022-04-19 16:17:43 +0300

    fix create empty node bug

--- database.py
+++ database.py
@@ -24,23 +24,23 @@ class DataBase:
         with self.driver.session() as session:
             try:
                 session.write_transaction(self.create_page_node, name)
-                print(f"the first node named {name} has been created")
+                print(f"the page node named {name} has been created")
             except Exception(exceptions.SessionExpired):
                 raise
 
-    def add_post(self, post_text, image):
+    def add_post(self, post_text, image, time):
         with self.driver.session() as session:
             try:
-                session.write_transaction(self.create_post_node, post_text, image)
+                session.write_transaction(self.create_post, post_text, image, time)
                 print(f"the post node has been created")
             except Exception(exceptions.SessionExpired):
                 raise
 
-    def add_relationship(self, page_name, post, time):
+    def add_relationship(self, page_name, time):
         with self.driver.session() as session:
             try:
-                session.write_transaction(self.create_relationship, page_name, post, time)
-                print(f"{page_name}--[{time}]-->{post}")
+                session.write_transaction(self.create_relationship, page_name, time)
+                print(f"{page_name}--[{time}]-->'post'")
             except Exception(exceptions.SessionExpired):
                 raise
 
@@ -51,35 +51,50 @@ class DataBase:
         except Exception(exceptions.CypherSyntaxError):
             raise
 
-    @staticmethod
-    def create_post_node(tx, post_text, image):
-        query = (
-            "CREATE (b:Post {Name: 'Post', Text: $text, Photo: $image})"
-            "RETURN b"
-        )
-        result = tx.run(query, text=post_text, image=image)
-        try:
-            return [{
-                "b": row["b"]["Post", "Text", "Photo"]
-            } for row in result]
+    # @staticmethod
+    # def create_post_node(tx, post_text, image, page_name, time):
+    #     query = (
+    #         "MATCH (a:Page) "
+    #         "WHERE a.name = $page "
+    #         "CREATE (b:Post {Name: 'Post', Text: $text, Photo: $image}) "
+    #         "CREATE (a)-[t:Date {Time: $time}]->(b) "
+    #         "RETURN a, b, t"
+    #     )
+    #     result = tx.run(query, text=post_text, image=image, page=page_name, time=time)
+    #     try:
+    #         # return result
+    #         return [{
+    #             "b": row["b"]["Post", "Text", "Photo"]
+    #         } for row in result]
+    #
+    #     except ServiceUnavailable as exception:
+    #         logging.error(f"{query} raised an error: \n {exception}")
+    #         raise
 
-        except ServiceUnavailable as exception:
-            logging.error(f"{query} raised an error: \n {exception}")
-            raise
-
     @staticmethod
-    def create_relationship(tx, page_name, post, time):
+    def create_relationship(tx, page_name, time):
         query = (
-            "MATCH (a: Page), (b: Post)"
-            "WHERE a.Name = $name"
-            "CREATE (a)-[t:Date {Time:$time}]->(b)"
-            "RETURN t;"
+            "MATCH (a: Page), (b: Post) "
+            "WHERE a.Name = $name AND b.Time = $time "
+            "CREATE (a)-[t:Date {Time:$time}]->(b) "
+            "RETURN t"
         )
-        result = tx.run(query, name=page_name, post=post, time=time)
+        result = tx.run(query, name=page_name, time=time)
         try:
             return result
         except ServiceUnavailable as exception:
             logging.error(f"{query} raised an error: \n {exception}")
             raise
 
-
+    @staticmethod
+    def create_post(tx, post_text, image, time):
+        query = (
+            "CREATE (b:Post {Name: 'Post', Text: $text, Photo: $image, Time: $time}) "
+            "RETURN b"
+        )
+        result = tx.run(query, text=post_text, image=image, time=time)
+        try:
+            return result
+        except ServiceUnavailable as exception:
+            logging.error(f"{query} raised an error: \n {exception}")
+            raise
