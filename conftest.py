a9d72b756c8130e6bbad7667dda7a365e586a17e
MaHDi <elmahdi.alagab@gmail.com>
2022-04-22 18:14:15 +0300

    create real scraped data for test

--- conftest.py
+++ conftest.py
@@ -1,20 +1,41 @@
 import datetime
 
-face = [{
-    'text': 'Everything you need to know to preorder Hogwarts Legacy, the upcoming open-world RPG set in '
-            'the wizarding world of Harry Potter, before the hero arrived.',
-    'photo': 'https://assets-prd.ignimgs.com/2022/03/18/hogwarts-legacy-4-1647622573681.jpeg?width=1280',
-    'time': datetime.datetime(2022, 3, 24, 16, 31, 9)}, {
-    'text': ' the upcoming open-world RPG set in '
-            'the wizarding world of Harry Potter, before the hero arrived.',
-    'photo': 'https://assets-prd.ignimgs.com/2022/03/18/hogwarts-legacy-4-1647622573681.jpeg?width=1280',
-    'time': datetime.datetime(2022, 2, 24, 20, 31, 9)},
-    {
-        'text': 'Everything you need to know to preorder Hogwarts Legacy, the upcoming open-world RPG set in '
-                'the wizarding',
-        'photo': 'https://assets-prd.ignimgs.com/2022/03/18/hogwarts-legacy-4-1647622573681.jpeg?width=1280',
-        'time': datetime.datetime(2022, 3, 4, 12, 31, 9)}
-]
+scraped_data = [{
+            'text': 'Welcome to the very first episode of Rogue Jam!\n\nOur judges are on the hunt for a game with '
+                    'Huge Potential, so join them as they tear through a Canadian wilderness rogue-like, '
+                    'a time-bending 2D shooter, and a hockey game with heart.',
+            'photo': 'http://s3.amazonaws.com/photos.sproutsocial.com/2022-04-14/2f1926ff-832a-4e26-81b7-ea8def6af7b4'
+                     '/6ca05102-a879-4fc3-aef9-a3e95e8ef9dc',
+            'time': datetime.datetime(2022, 4, 18, 19, 2, 5)},
 
-for post in face:
+        {
+            'text': 'Gearbox Publishing has announced Eyes in the Dark: The Curious Case of One Victoria Bloom, '
+                    'an atmospheric roguelight coming to PC on July 14.',
+            'photo': 'https://assets-prd.ignimgs.com/2022/04/20/eyes-in-the-dark-key-art-1650467328107.png?width=1280',
+            'time': datetime.datetime(2022, 4, 22, 17, 30, 23)},
+
+        {
+            'text': 'Doctor Strange in the Multiverse of Madness writer Michael Waldron has acknowledged the dangers '
+                    'of creating the MCU multiverse and the considerations behind such an expansion.',
+            'photo': 'https://assets-prd.ignimgs.com/2022/04/21/doctor-strange-mcu-multiverse-1650544967965.jpg?width=1280',
+            'time': datetime.datetime(2022, 4, 22, 17, 0, 52)},
+
+        {'text': 'Torchlight 2 enters its latest closed beta on April 24.',
+         'photo': 'https://assets-prd.ignimgs.com/2022/04/20/1920-1080-without-tap-logo-1650482192689.jpg?width=1280',
+         'time': datetime.datetime(2022, 4, 22, 16, 30, 20)},
+
+        {
+            'text': 'The House of the Dead: Remake is coming to PC, PS4, Xbox One, and Stadia just weeks after the '
+                    'game was released exclusively on Nintendo Switch.',
+            'photo': 'https://assets-prd.ignimgs.com/2022/04/05/houseofthedeadremake-review-blogroll-1649185073762'
+                     '.jpg?width=1280',
+            'time': datetime.datetime(2022, 4, 22, 16, 0, 27)},
+
+        {'text': 'What is *your* favorite co-op game of all time?',
+         'photo': 'https://assets-prd.ignimgs.com/2021/09/15/coop-blogroll-1631726339807.jpg?width=1280',
+         'time': datetime.datetime(2022, 4, 22, 15, 30, 19)}
+
+        ]
+
+for post in scraped_data:
     pass
