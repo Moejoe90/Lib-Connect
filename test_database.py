650508d3901548451a55db7acf1627866faa5f7a
MaHDi <elmahdi.alagab@gmail.com>
2022-04-22 18:11:39 +0300

    test create node

--- test_database.py
+++ test_database.py
@@ -1,30 +1,26 @@
 from database import DataBase
-from scrap import Post
+from conftest import scraped_data
 from settings import TEST_USER, TEST_URI, TEST_PASSWORD
-
 
 PAGE_NAME = 'GameSpot'
 
 
 class TestDataBase:
 
-    def __int__(self):
-        self.data = DataBase(uri=TEST_URI, user=TEST_USER, password=TEST_PASSWORD)
-        self.facebook = Post(page_name=PAGE_NAME, num_pages=10)
-
     def test_connection(self):
         driver = DataBase(uri=TEST_URI, user=TEST_USER, password=TEST_PASSWORD)
         verify = driver.check_connection()
         assert verify is None
 
-    # TODO
     def test_create_nodes(self):
         driver = DataBase(uri=TEST_URI, user=TEST_USER, password=TEST_PASSWORD)
         driver.add_page(PAGE_NAME)
-        for post in self.facebook.scrape_full_post():
+        for post in scraped_data:
             driver.add_post(post_text=post['text'], time=post['time'], image=post['photo'])
             driver.add_relationship(page_name=PAGE_NAME, time=post['time'])
+        driver.close()
+        assert driver.fine_page(PAGE_NAME)['page_found'] == PAGE_NAME
 
-    # TODO
+    # TODO check the last node
     def test_exist_nodes(self):
         pass
