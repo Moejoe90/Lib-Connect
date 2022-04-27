fb6a52c0db59a7864153ddf869c890f535678580
MaHDi <elmahdi.alagab@gmail.com>
2022-04-27 22:58:50 +0300

    stracture image class

---
+++ image.py
@@ -0,0 +1,23 @@
+from PIL import Image
+import requests
+import os
+import io
+
+
+class ImageManagement(object):
+
+    # TODO implement the AWS auth
+    def __int__(self):
+        pass
+
+    # TODO download the image and save it temporary
+    def download(self):
+        pass
+
+    # TODO upload the image to the AWS
+    def upload(self):
+        pass
+
+    # TODO generate small link from the real AWS image link
+    def link_image(self):
+        pass
