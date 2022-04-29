48069c3dd21eaa99ba75eb0ae676868e050731c0
MaHDi <elmahdi.alagab@gmail.com>
2022-04-29 19:02:40 +0300

    uplaod image successful

--- image.py
+++ image.py
@@ -4,12 +4,13 @@ from settings import AWS_ACCESS_KEY, AWS_SECRET_KEY, BUCKET_NAME
 import requests
 import boto3
 from botocore.exceptions import ClientError
+from boto3.s3.transfer import S3UploadFailedError
 import logging
 import tempfile
 import io
 
 logger = logging.getLogger(__name__)
-ROOT = Path(__file__).parent
+ROOT = Path(__file__).parent.resolve()
 
 
 class ImageManagement(object):
@@ -29,9 +30,17 @@ class ImageManagement(object):
             print(self.temp_dir)
             print(ROOT)
 
-    # TODO upload the image to the AWS
-    def upload(self):
-        pass
+    def upload(self, image_name, bucket, object_name=None):
+        self.s3 = self.session.client('s3')
+        if object_name is None:
+            object_name = image_name
+        if self.s3 is not None:
+            try:
+                self.s3.upload_file(image_name, bucket, object_name)
+                print(f"{image_name} has been uploaded to {BUCKET_NAME}")
+            except S3UploadFailedError:
+                raise
+        # TODO return s3 url
 
     # TODO generate small link from the real AWS image link
     def link_image(self):
